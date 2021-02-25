fece296ec0c3679d1ee142b63233cfe0d1d8d78a
braedonb98 <brbe3574@colorado.edu>
2021-02-24 20:03:04 -0700

    finished assignment

--- src/book.js
+++ src/book.js
@@ -1,12 +1,55 @@
-function createTitle() {
-  
+
+function createTitle(title) {
+  return ("The " + title);
+}
+
+function buildMainCharacter(name, age, pronouns)
+{
+  var character = {};
+  character.name = name;
+  character.age = age;
+  character.pronouns = pronouns;
+  return(character);
+}
+
+function saveReview(review, reviews)
+{
+  same = 0;
+  for(i in reviews)
+  {
+    if(review == reviews[i]){
+      return reviews;
+    }
+  }
+reviews[reviews.length]= review;
+return(reviews);
+}
+
+function calculatePageCount(title)
+{
+  return (title.length*20);
+}
+
+function writeBook(title, character, genre)
+{
+  var book = {};
+  book.title = title;
+  book.mainCharacter = character;
+  book.genre = genre;
+  book.pageCount = calculatePageCount(title);
+  return book;
+}
+
+function editBook(book)
+{
+book.pageCount = book.pageCount * 0.75;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
